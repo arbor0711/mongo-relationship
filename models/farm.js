7afef93d4483e2137ebd99b2336073c5aa66887e
Alireza <arbor0711@gmail.com>
2023-03-14 22:05:26 +0100

    Creating addProduct function

---
+++ models/farm.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+// destruction Schema from mongoose
+const { Schema } = mongoose;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/relationshipDemo", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!");
+  })
+  .catch((err) => {
+    console.log("MONGO CONNECTION FAILED!!!");
+    console.log(err);
+  });
+
+//   defining child model -> product
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  season: {
+    type: String,
+    enum: ["Spring", "Summer", "Fall", "Winter"],
+  },
+});
+
+// make model
+const Product = mongoose.model("Product", productSchema);
+
+// make a couple of products
+// Product.insertMany([
+//   { name: "Apple", price: 1, season: "Spring" },
+//   { name: "Orange", price: 2, season: "Fall" },
+//   { name: "Tomato", price: 1, season: "Summer" },
+//   { name: "Potato", price: 1, season: "Winter" },
+//   { name: "Cherry", price: 1, season: "Summer" },
+// ]);
+
+//   create second model-> farm model using Schema instead of mongoose.Schema
+const farmSchema = new Schema({
+  name: String,
+  city: String,
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+});
+
+// make model
+const Farm = mongoose.model("Farm", farmSchema);
+// make a farm
+const makeFarm = async () => {
+  const farm = new Farm({
+    name: "Wensen Farm",
+    city: "Zarghan",
+  });
+  const apple = await Product.findOne({ name: "Apple" });
+  farm.products.push(apple);
+  const res = await farm.save();
+};
+
+const addProduct = async (place, item) => {
+  const farm = await Farm.findOne({ name: place });
+  const product = await Product.findOne({ name: item });
+  farm.products.push(product);
+  const res = await farm.save();
+  console.log(res);
+};
+
+addProduct("Wensen Farm", "Potato");
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // connect to database
 mongoose
-  .connect("mongodb://localhost:27017/relationshipDemo", {
+  .connect("mongodb://127.0.0.1:27017/relationshipDemo", {
     useNewUrlParser: true,
   })
   .then(() => {
@@ -13,4 +13,51 @@ mongoose
     console.log(err);
   });
 
-//   define schema for user
+// define schema for user
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  address: [
+    {
+      _id: { _id: false },
+      street: String,
+      city: String,
+      state: String,
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+// make model user
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+  const u = new User({
+    first: "Tom",
+    last: "Keshi",
+  });
+  u.address.push({
+    street: "123 Galvanistraat",
+    city: "Den Haag",
+    state: "North Holland",
+    country: "USA",
+  });
+  const res = await u.save();
+  console.log(res);
+};
+
+const addAddress = async (id) => {
+  const u = await User.findById(id);
+  u.address.push({
+    street: "13 faz",
+    city: "Ray",
+    state: "Fars",
+    country: "Iran",
+  });
+  const res = await u.save();
+};
+
+addAddress("6410a6b12ccba4269a260d28");
